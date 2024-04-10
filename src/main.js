fd80a679f3d09a187bbb704c6b29d63f966cd79a
nextnode-managedcode <39308621+nextnode-managedcode@users.noreply.github.com>
2024-04-10 13:36:23 -0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import App from "./App.vue";
+import VueGoodTablePlugin from "vue-good-table";
+import "vue-good-table/dist/vue-good-table.css";
+
+Vue.config.productionTip = false;
+Vue.use(VueGoodTablePlugin);
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
